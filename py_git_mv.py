ebe37b5c73d357c3e4dfadddc842747e451ba346
mimin0 <vitalik.dp@gmail.com>
2016-10-17 22:39:53 +0200

    added creting of folders and files

--- py_git_mv.py
+++ py_git_mv.py
@@ -3,13 +3,19 @@
 #from subprocess import call
 import os
 
-##Create list of file
-def createStructure():
+##Create list of files and folders
+def createOldStructure():
 	os.system('mkdir -pv test/A/A1 test/B/B1 test/C/C1')
 	print "INFO :: folders created"
-	
+	os.system('touch test/A/a.aa test/B/b.bb test/C/c.cc test/A/A1/a1.aa test/B/B1/b1.bb test/C/C1/c1.cc test/A/A1/a2.aa test/B/B1/b2.bb test/C/C1/c2.cc')
+	print "INFO :: files created"
 
-createStructure()
+def createNewStructure():
+	os.system('mkdir -pv testNew/AN/A1N testNew/BN/B1N testNew/CN/C1N')
+	print "INFO :: folders created - NEW"
+
+createOldStructure()
+createNewStructure()
 
 ##Read list of file into array
 
