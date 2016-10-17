cc9a3b33db73ee06f8b4fc5532bfdc9c8e93c5a4
mimin0 <vitalik.dp@gmail.com>
2016-10-17 22:22:34 +0200

    first commit

---
+++ py_git_mv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#from subprocess import call
+import os
+
+##Create list of file
+def createStructure():
+	os.system('mkdir -pv test/A/A1 test/B/B1 test/C/C1')
+	print "INFO :: folders created"
+	
+
+createStructure()
+
+##Read list of file into array
+
+
+##Compare files and show result
+
+
+## GIT move
+
+
+## Show resul of movement 
